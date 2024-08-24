514d8b10f891597185f1fc410617e8465c821c84
LuisDev05 <luisfernandogoncalvesmendes5@gmail.com>
2024-08-23 23:18:49 -0400

    Portifolio 3.1

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let menulcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks =  document.querySelectorAll('header nav a');
 
+// Get the menu icon and navbar elements
+const menuIcon = document.getElementById('#menu-icon');
+const navbar = document.querySelector('.navbar');
 
+// Add an event listener to the menu icon
+menuIcon.addEventListener('click', () => {
+  // Toggle the active class on the navbar
+  navbar.classList.toggle('active');
+});
 window.onscroll = () => {
     sections.forEach(sec =>  {
         let top = window.scrollY;
