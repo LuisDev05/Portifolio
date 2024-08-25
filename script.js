8d5b3a383a1bb78bc3d0d301314964f3a8187cd6
LuisDev05 <luisfernandogoncalvesmendes5@gmail.com>
2024-08-24 23:03:33 -0400

    Portifolio

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 let menulcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks =  document.querySelectorAll('header nav a');
 
-// Get the menu icon and navbar elements
-const menuIcon = document.getElementById('#menu-icon');
-const navbar = document.querySelector('.navbar');
 
-// Add an event listener to the menu icon
-menuIcon.addEventListener('click', () => {
-  // Toggle the active class on the navbar
-  navbar.classList.toggle('active');
-});
 window.onscroll = () => {
     sections.forEach(sec =>  {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.Attribute('id');
+        let id = sec.getAttribute('id');
     
         if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
@@ -28,6 +21,5 @@ window.onscroll = () => {
 }
 menulcon.onclick = () => {
     menulcon.classList.toggle('bx-x');
-    navbar.classList('active')
+    navbar.classList.toggle('active');
 }
-
